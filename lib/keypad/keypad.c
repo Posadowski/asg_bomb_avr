8b66f632fec2616ba06cdbcfd1ce04a50db1081f
Posadowski <mateuszposa@gmail.com>
2024-03-16 17:16:21 +0100

    fix: keyboad handler

--- lib/keypad/keypad.c
+++ lib/keypad/keypad.c
@@ -1,15 +1,127 @@
 #include "keypad.h"
 
+#include <avr/io.h>
+#include <util/delay.h>
+
 void keypad_init(){
 
- // Konfiguracja C1...C4 jako wyjścia i ustawienie na HIGH
-    DDRB |= (1 << C1);  // C1
-    PORTB |= (1 << C1);
+ // Configure C1...C4 as outputs and set to HIGH
+    DDRB |= (1 << C1_PIN);  // C1
+    PORTB |= (1 << C1_PIN);
 
-    DDRD |= (1 << C4) | (1 << C3) | (1 << C2);  // C2, C3, C4
-    PORTD |= (1 << C4) | (1 << C3) | (1 << C2);
+    DDRD |= (1 << C4_PIN) | (1 << C3_PIN) | (1 << C2_PIN);  // C2, C3, C4
+    PORTD |= (1 << C4_PIN) | (1 << C3_PIN) | (1 << C2_PIN);
 
-    // Konfiguracja R1...R4 jako wejścia z pull-up
-    DDRB &= ~((1 << R4) | (1 << R3) | (1 << R2) | (1 << R1));  // R1...R4
-    PORTB |= (1 << R4) | (1 << R3) | (1 << R2) | (1 << R1);  // Włączenie pull-up
+    // Configuration of R1...R4 as pull-up inputs
+    DDRB &= ~((1 << R4_PIN) | (1 << R3_PIN) | (1 << R2_PIN) | (1 << R1_PIN));  // R1...R4
+    PORTB |= (1 << R4_PIN) | (1 << R3_PIN) | (1 << R2_PIN) | (1 << R1_PIN);  // Enable pull-up
+
+}
+
+
+void keypad_tick(){
+    for (int i = 0; i < COLS; i++) {
+        keypad_columns_ddr[i] |= (1 << keypad_columns_pin[i]); // set HIGH on colums 
+    }
+
+    uint8_t i = 0;
+    for (uint8_t c = 0; c < COLS; c++) {
+       keypad_columns_port[i] &= ~(1 << keypad_columns_pin[i]); //set LOW
+
+       _delay_ms(_KEYPAD_SETTLING_DELAY);
+    for (uint8_t r = 0; r < ROWS; r++) {
+      i = r * COLS + c;
+      
+      if(!(PINB & (1 << keypad_rows_pin[r]))){ // read state from row
+        keyboard_event.state = _JUST_PRESSED;
+        keyboard_event.column = c;
+        keyboard_event.row = r;
+      }
+    }
+    keypad_columns_ddr[c] |= (1 << keypad_columns_pin[c]); // set HIGH 
+  }
+}
+
+char keyboard_check_key_pressed() {
+  
+    for (int col = 0; col < 4; col++) {
+        switch (col) {
+            case 0:
+                C1_PORT &= ~(1 << C1_PIN);
+                C2_PORT |= (1 << C2_PIN);
+                C3_PORT |= (1 << C3_PIN);
+                C4_PORT |= (1 << C4_PIN);
+                break;
+            case 1:
+                C1_PORT |= (1 << C1_PIN);
+                C2_PORT &= ~(1 << C2_PIN);
+                C3_PORT |= (1 << C3_PIN);
+                C4_PORT |= (1 << C4_PIN);
+                break;
+            case 2:
+                C1_PORT |= (1 << C1_PIN);
+                C2_PORT |= (1 << C2_PIN);
+                C3_PORT &= ~(1 << C3_PIN);
+                C4_PORT |= (1 << C4_PIN);
+                break;
+            case 3:
+                C1_PORT |= (1 << C1_PIN);
+                C2_PORT |= (1 << C2_PIN);
+                C3_PORT |= (1 << C3_PIN);
+                C4_PORT &= ~(1 << C4_PIN);
+                break;
+        }
+        
+        
+        for (int row = 0; row < 4; row++) {
+            switch (row) {
+                case 0:
+                  if(col == 0){
+                    if (!(PINB & (1 << R1_PIN))) return '1';
+                  } else if (col == 1){
+                    if (!(PINB & (1 << R1_PIN))) return '2';
+                  } else if (col == 2){
+                    if (!(PINB & (1 << R1_PIN))) return '3';
+                  } else if (col == 3){
+                    if (!(PINB & (1 << R1_PIN))) return 'A';
+                  } 
+                    break;
+                case 1:
+                  if (col == 0){
+                    if (!(PINB & (1 << R2_PIN))) return '4';
+                  } else if (col == 1){
+                    if (!(PINB & (1 << R2_PIN))) return '5';
+                  } else if (col == 2){
+                    if (!(PINB & (1 << R2_PIN))) return '6';
+                  } else if (col == 3){
+                    if (!(PINB & (1 << R2_PIN))) return 'B';
+                  }  
+                    
+                    break;
+                case 2:
+                  if (col == 0){
+                    if (!(PINB & (1 << R3_PIN))) return '7';
+                  } else if (col == 1){
+                    if (!(PINB & (1 << R3_PIN))) return '8';
+                  } else if (col == 2){
+                    if (!(PINB & (1 << R3_PIN))) return '9';
+                  } else if (col == 3){
+                    if (!(PINB & (1 << R3_PIN))) return 'C';
+                  } 
+                    break;
+                case 3:
+                  if (col == 0){
+                    if (!(PINB & (1 << R4_PIN))) return '*';
+                  } else if (col == 1){
+                    if (!(PINB & (1 << R4_PIN))) return '0';
+                  } else if (col == 2){
+                    if (!(PINB & (1 << R4_PIN))) return '#';
+                  } else if (col == 3){
+                    if (!(PINB & (1 << R4_PIN))) return 'D';
+                  }    
+                    break;
+            }
+        }
+    }
+    return '\0'; //If there is no button pressed
 }
--- lib/keypad/keypad.h
+++ lib/keypad/keypad.h
@@ -4,20 +4,58 @@
 #include <avr/io.h>
 
 // define your keyboar pins here
-#define R1      PB4
-#define R2      PB3
-#define R3      PB2
-#define R4      PB1
-#define C1      PB0
-#define C2      PD7
-#define C3      PD6
-#define C4      PD5
+//rows
+#define R1_PIN      PB4
+#define R1_PORT     PORTB
+#define R1_DDR      DDRB
+
+#define R2_PIN      PB3
+#define R2_PORT     PORTB
+#define R2_DDR      DDRB
+
+#define R3_PIN      PB2
+#define R3_PORT     PORTB
+#define R3_DDR      DDRB
+
+#define R4_PIN      PB1
+#define R4_PORT     PORTB
+#define R4_DDR      DDRB
+
+//colums
+#define C1_PIN      PB0
+#define C1_PORT     PORTB
+#define C1_DDR      DDRB
+
+#define C2_PIN      PD7
+#define C2_PORT     PORTD
+#define C2_DDR      DDRD
+
+#define C3_PIN      PD6
+#define C3_PORT     PORTD
+#define C3_DDR      DDRD
+
+#define C4_PIN      PD5
+#define C4_PORT     PORTD
+#define C4_DDR      DDRD
 
 #define ROWS    4
 #define COLS    4 
 
-//initialize an instance of class NewKeypad
+#define _KEY_PRESSED_POS (1)
+#define _KEY_PRESSED (1UL << _KEY_PRESSED_POS)
+
+#define _JUST_PRESSED_POS (2)
+#define _JUST_PRESSED (1UL << _JUST_PRESSED_POS)
+
+#define _JUST_RELEASED_POS (3)
+#define _JUST_RELEASED (1UL << _JUST_RELEASED_POS)
+
+#define KEY_JUST_RELEASED (0) ///< key has been released
+#define KEY_JUST_PRESSED (1)  ///< key has been pressed
+
+#define _KEYPAD_SETTLING_DELAY 20
 
 void keypad_init();
+char keyboard_check_key_pressed();
 
 #endif //KEYPAD_H_
