727b14204e1a37875d466927ff4ae2338852f9ff
Posadowski <mateuszposa@gmail.com>
2024-03-17 12:12:57 +0100

    chore: delete unused defines

--- lib/keypad/keypad.h
+++ lib/keypad/keypad.h
@@ -6,37 +6,14 @@
 // define your keyboar pins here
 // rows
 #define R1_PIN PB4
-#define R1_PORT PORTB
-#define R1_DDR DDRB
-
 #define R2_PIN PB3
-#define R2_PORT PORTB
-#define R2_DDR DDRB
-
 #define R3_PIN PB2
-#define R3_PORT PORTB
-#define R3_DDR DDRB
-
 #define R4_PIN PB1
-#define R4_PORT PORTB
-#define R4_DDR DDRB
-
 // colums
 #define C1_PIN PB0
-#define C1_PORT PORTB
-#define C1_DDR DDRB
-
 #define C2_PIN PD7
-#define C2_PORT PORTD
-#define C2_DDR DDRD
-
 #define C3_PIN PD6
-#define C3_PORT PORTD
-#define C3_DDR DDRD
-
 #define C4_PIN PD5
-#define C4_PORT PORTD
-#define C4_DDR DDRD
 
 #define ROWS 4
 #define COLS 4
@@ -45,6 +22,11 @@
 #define KEYPAD_RELEASES 0
 
 #define _KEYPAD_CHECK_TIME 5
+
+#define ENTER_KEY 'D'
+#define PRESET_1_KEY 'A'
+#define PRESET_2_KEY 'B'
+#define PRESET_3_KEY 'C'
 
 void keypad_init();
 void keypad_check_key_pressed(void *arg);
